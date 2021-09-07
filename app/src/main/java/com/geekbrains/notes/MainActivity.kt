118f9d5cbd75e4fe72d86f42059a51bc741dcf78
svr <svr@protonmail.com>
2021-09-08 00:41:24 +0300

    Обработка нажатия на About

--- app/src/main/java/com/geekbrains/notes/MainActivity.kt
+++ app/src/main/java/com/geekbrains/notes/MainActivity.kt
@@ -2,7 +2,10 @@ package com.geekbrains.notes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         model.removedItem.observe(this, { id -> showListFragment() })
         model.modifiedNoteId.observe(this, { id -> showListFragment() })
         setContentView(R.layout.activity_main)
+        findViewById<Toolbar>(R.id.main_toolbar).setOnMenuItemClickListener({ item -> handleMenu(item) })
         if (isPortrait) {
             supportFragmentManager.beginTransaction().hide(detailFrag()).show(listFrag()).commit()
         } else {
@@ -37,4 +41,12 @@ class MainActivity : AppCompatActivity() {
 
     fun listFrag(): Fragment = supportFragmentManager.findFragmentByTag("list_fragment_tag")!!
     fun detailFrag(): Fragment = supportFragmentManager.findFragmentByTag("details_fragment_tag")!!
+
+    fun handleMenu(item: MenuItem): Boolean {
+        if (item.itemId == R.id.about) {
+            Toast.makeText(this, R.string.unimplemented, Toast.LENGTH_SHORT).show()
+            return true
+        }
+        return false
+    }
 }
