60b27bebd5c14695233c01173fed88a07bc81c46
svr <svr@protonmail.com>
2021-09-14 23:15:53 +0300

    Косметика

--- app/src/main/java/com/geekbrains/notes/MainViewModel.kt
+++ app/src/main/java/com/geekbrains/notes/MainViewModel.kt
@@ -107,7 +107,7 @@ class MainViewModel : ViewModel() {
 
     private fun setupEditNew(index: Int) {
         if (index < 0 || index > items.size) { // при вставке индекс равный size валиден
-            // специальны случай -1 допускается
+            // специальный случай -1 допускается
             assert(index == -1, { "incorrect usage of setupEditNew i=$index is not in [0,${items.size}]" })
             setupEditingDisabled()
         } else {
@@ -117,7 +117,7 @@ class MainViewModel : ViewModel() {
 
     private fun setupEditOld(index: Int) {
         if (index < 0 || index >= items.size) { // при редактировании максимальный индекс size - 1
-            // специальны случай -1 допускается
+            // специальный случай -1 допускается
             assert(index == -1, { "incorrect usage of setupEditOld i=$index is not in [0,${items.size})" })
             setupEditingDisabled()
         } else {
